e88c436d9dcbaa8c32c945c8568a45473d56c3e6
Daniel Alan Miller <dalan.miller@gmail.com>
2013-10-14 11:10:59 +1030

    Saving changes before turning in Mac for repair

--- api.py
+++ api.py
@@ -1,11 +1,22 @@
 
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+	if request.method == 'POST':
+		session['username'] = request.form['username']
+		return redirect(url_for('index'))
+
+
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
